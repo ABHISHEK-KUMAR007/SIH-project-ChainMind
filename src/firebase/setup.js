cf1d263ed3cab52163c9d3277305f6271bbded27
Sourav <sourav.khilari.it26@heritageit.edu.in>
2024-09-06 02:08:09 +0530

    demo_app_update_firebase

--- src/firebase/setup.js
+++ src/firebase/setup.js
@@ -1,23 +1,23 @@
-// import { initializeApp } from "firebase/app";
-// import {getAuth,GoogleAuthProvider} from "firebase/auth"
-// import {getFirestore} from "firebase/firestore"
-// import { getStorage } from "firebase/storage"; 
+import { initializeApp } from "firebase/app";
+import {getAuth,GoogleAuthProvider} from "firebase/auth"
+import {getFirestore} from "firebase/firestore"
+import { getStorage } from "firebase/storage"; 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAIkPNVf0AbZOhSLGc4RLnUQy1RpV8_VfQ",
-//   authDomain: "whatsapp-clone-e31bb.firebaseapp.com",
-//   projectId: "whatsapp-clone-e31bb",
-//   storageBucket: "whatsapp-clone-e31bb.appspot.com",
-//   messagingSenderId: "920175657952",
-//   appId: "1:920175657952:web:c12d8205cc3da95bfba185"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyAIkPNVf0AbZOhSLGc4RLnUQy1RpV8_VfQ",
+  authDomain: "whatsapp-clone-e31bb.firebaseapp.com",
+  projectId: "whatsapp-clone-e31bb",
+  storageBucket: "whatsapp-clone-e31bb.appspot.com",
+  messagingSenderId: "920175657952",
+  appId: "1:920175657952:web:c12d8205cc3da95bfba185"
+};
 
 
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app)
-// export const googleProvider = new GoogleAuthProvider(app)
-// export const database = getFirestore(app)
-// export const storage = getStorage(app);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider(app)
+export const database = getFirestore(app)
+export const storage = getStorage(app);
 
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
@@ -50,27 +50,27 @@
 
 //2nd account
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth,GoogleAuthProvider} from "firebase/auth"
-import {getFirestore} from "firebase/firestore"
-import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "firebase/app";
+// import {getAuth,GoogleAuthProvider} from "firebase/auth"
+// import {getFirestore} from "firebase/firestore"
+// import { getStorage } from "firebase/storage";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAIlvsN4-nhRy02c8bPuZCwJLYBaEHqcUw",
-  authDomain: "fir-chat-43600.firebaseapp.com",
-  projectId: "fir-chat-43600",
-  storageBucket: "fir-chat-43600.appspot.com",
-  messagingSenderId: "648158221231",
-  appId: "1:648158221231:web:d8d41902502917dd5d155a"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAIlvsN4-nhRy02c8bPuZCwJLYBaEHqcUw",
+//   authDomain: "fir-chat-43600.firebaseapp.com",
+//   projectId: "fir-chat-43600",
+//   storageBucket: "fir-chat-43600.appspot.com",
+//   messagingSenderId: "648158221231",
+//   appId: "1:648158221231:web:d8d41902502917dd5d155a"
+// };
 
-// Initialize Firebase
+// // Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const googleProvider = new GoogleAuthProvider(app)
-export const database = getFirestore(app)
-export const storage = getStorage(app);
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app)
+// export const googleProvider = new GoogleAuthProvider(app)
+// export const database = getFirestore(app)
+// export const storage = getStorage(app);
